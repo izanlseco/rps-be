ea02944d0e2355666b72cd8ccc3ad2c0602bdac2
Izan Lopez Seco <IzanLS09@gmail.com>
2022-01-23 13:53:32 +0100

    Initial commit

---
+++ src/main/java/com/task/rpsbe/RpsBeApplication.java
@@ -0,0 +1,11 @@
+package com.task.rpsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsBeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RpsBeApplication.class, args);
+	}
+}
