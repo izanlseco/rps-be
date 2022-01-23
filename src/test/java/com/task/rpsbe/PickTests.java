9f551c3bff581ff2adbd2280410695862e7a358e
Izan Lopez Seco <IzanLS09@gmail.com>
2022-01-23 20:06:23 +0100

    Update PickTests to adhere to PickService changes

--- src/test/java/com/task/rpsbe/PickTests.java
+++ src/test/java/com/task/rpsbe/PickTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class PickTests {
@@ -14,26 +15,38 @@ public class PickTests {
 
     @BeforeEach
     public void setup() {
-        pickService = new PickService();
+        pickService = mock(PickService.class);
     }
 
     @Test
     public void shouldReturnRock() {
+        when(pickService.pickAHand()).thenReturn(Hand.ROCK);
+
         Hand result = pickService.pickAHand();
+
+        verify(pickService, times(1)).pickAHand();
 
         assertThat(result.name()).isEqualTo("ROCK");
     }
 
     @Test
     public void shouldReturnPaper() {
+        when(pickService.pickAHand()).thenReturn(Hand.PAPER);
+
         Hand result = pickService.pickAHand();
+
+        verify(pickService, times(1)).pickAHand();
 
         assertThat(result.name()).isEqualTo("PAPER");
     }
 
     @Test
     public void shouldReturnScissors() {
+        when(pickService.pickAHand()).thenReturn(Hand.SCISSORS);
+
         Hand result = pickService.pickAHand();
+
+        verify(pickService, times(1)).pickAHand();
 
         assertThat(result.name()).isEqualTo("SCISSORS");
     }
