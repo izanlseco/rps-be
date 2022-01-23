22e9d9b2fea3fe5177d403becf65ce8da067c4e2
Izan Lopez Seco <IzanLS09@gmail.com>
2022-01-23 21:44:48 +0100

    Rename KarateTest -> KarateTests

--- src/test/java/com/task/rpsbe/karate/KarateTests.java
+++ src/test/java/com/task/rpsbe/karate/KarateTests.java
@@ -2,7 +2,7 @@ package com.task.rpsbe.karate;
 
 import com.intuit.karate.junit5.Karate;
 
-public class KarateTest {
+public class KarateTests {
     @Karate.Test
     Karate testPickController() {
         return Karate.run("play_hand").relativeTo(getClass());
