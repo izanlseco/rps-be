e5c2e1b512a2b02433f2c1e395da76f52e53d338
Izan Lopez Seco <IzanLS09@gmail.com>
2022-01-24 12:13:55 +0100

    Update PlayController so it updates the score after playing

--- src/main/java/com/task/rpsbe/controller/PlayController.java
+++ src/main/java/com/task/rpsbe/controller/PlayController.java
@@ -3,6 +3,7 @@ package com.task.rpsbe.controller;
 import com.task.rpsbe.model.Hand;
 import com.task.rpsbe.service.IPickService;
 import com.task.rpsbe.service.IPlayService;
+import com.task.rpsbe.service.IScoreService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +16,21 @@ public class PlayController {
 
     private final IPickService iPickService;
     private final IPlayService iPlayService;
+    private final IScoreService iScoreService;
 
-    private PlayController(final IPickService iPickService, final IPlayService iPlayService) {
+    private PlayController(final IPickService iPickService,
+                           final IPlayService iPlayService,
+                           final IScoreService iScoreService) {
         this.iPickService = iPickService;
         this.iPlayService = iPlayService;
+        this.iScoreService = iScoreService;
     }
 
     @RequestMapping(value = "/play", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public int playAHand() {
-        log.debug("Picking a hand with PickService");
         Hand playerTwoHand = iPickService.pickAHand();
-        return iPlayService.playAHand(playerTwoHand);
+        int resultOfGame = iPlayService.playAHand(playerTwoHand);
+        iScoreService.updateScore(resultOfGame);
+        return resultOfGame;
     }
 }
