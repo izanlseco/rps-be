a824cc355bde92d11fcefbcf7be5e0a7c6c7b329
Izan Lopez Seco <IzanLS09@gmail.com>
2022-02-08 09:49:36 +0100

    Update controller with POST for better REST adhesion Update play feature with POST Add @CrossOrigin for local deployment of both front-end and back-end

--- src/main/java/com/task/rpsbe/controller/PlayController.java
+++ src/main/java/com/task/rpsbe/controller/PlayController.java
@@ -6,7 +6,8 @@ import com.task.rpsbe.service.IPlayService;
 import com.task.rpsbe.service.IScoreService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,8 @@ public class PlayController {
     private final IPlayService iPlayService;
     private final IScoreService iScoreService;
 
-    @GetMapping(value = "/play", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:3000/")
+    @PostMapping(value = "/play", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public int playAHand() {
         Hand playerTwoHand = iPickService.pickAHand();
         int resultOfGame = iPlayService.playAHand(playerTwoHand);
--- src/test/java/com/task/rpsbe/karate/play.feature
+++ src/test/java/com/task/rpsbe/karate/play.feature
@@ -1,6 +1,12 @@
 Feature: Play validation
+  Background:
+    * def emptyBody =
+    """
+    {}
+    """
   Scenario: when '/play' endpoint is called 200 is returned
     Given url baseUrl
+    And request emptyBody
     And path 'play'
-    When method GET
+    When method POST
     Then status 200
