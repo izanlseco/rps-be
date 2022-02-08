9bb5928235c9adf7eb4b7b9370225bd94b8956d7
Izan Lopez Seco <IzanLS09@gmail.com>
2022-02-08 09:49:44 +0100

    Add @CrossOrigin for local deployment of both front-end and back-end

--- src/main/java/com/task/rpsbe/controller/ScoreController.java
+++ src/main/java/com/task/rpsbe/controller/ScoreController.java
@@ -4,6 +4,7 @@ import com.task.rpsbe.model.Score;
 import com.task.rpsbe.service.IScoreService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScoreController {
     private final IScoreService iScoreService;
 
+    @CrossOrigin(origins = "http://localhost:3000/")
     @GetMapping(value = "/score", produces = MediaType.APPLICATION_JSON_VALUE)
     public Score getScore() {
         return iScoreService.getScore();
